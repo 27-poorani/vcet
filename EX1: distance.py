adf2c47084153b807f64fad331a69da4ccfc05fc
27-poorani <121332600+27-poorani@users.noreply.github.com>
2022-12-25 08:05:02 +0530

    Create EX1: distance.py

---
+++ EX1: distance.py
@@ -0,0 +1,22 @@
+INPUT
+a=(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=(int(input("enter the value of c"))
+d=int(input("enter the value of d"))
+x1=a
+x2=b
+y1=c
+y2=d
+d=((x1-x2)**2+(y1-y2)**2)**1/2
+print("the distance is",d)
+   
+ OUTPUT
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:/Users/rajkumar/Desktop/sap/distance.py ==============
+enter the value of a10
+enter the value of b20
+enter the value of c30
+enter the value of d40
+the distance is 100.0
